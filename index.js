e9b49c7f6574af074fb4c3d9f9536ad6e2cc78d0
Patrick Hart <pathart612@gmail.com>
2022-04-04 21:07:21 -0400

    commit tree 45ef20cb53866f58455884862494c2d4952271ed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function  distanceFromHqInBlocks(customerBlock){
+    if (customerBlock >= 42){
+        return customerBlock - 42;
+    } else {
+        return 42 - customerBlock;
+    }
+}
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, stop){
+    if (start > stop) {
+        return (start-stop) * 264;
+    } 
+    else {
+        return (stop - start) * 264;
+    }
+}
+
+function calculatesFarePrice(start, stop){
+    let dist = distanceTravelledInFeet(start, stop);
+
+    if (dist <= 400){
+        return 0;
+    }
+    else if (dist <= 2000){
+        return (dist - 400) * 0.02;
+    }
+    else if (dist <= 2500){
+        return 25
+    }
+    else{
+        return "cannot travel that far"
+    }
+}
